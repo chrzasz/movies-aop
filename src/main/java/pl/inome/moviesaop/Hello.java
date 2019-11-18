79eda1aece10430f8f397810133223574d405d22
Greg <chrzaszczyk@gmail.com>
2019-11-18 13:17:44 +0100

    dependency

---
+++ src/main/java/pl/inome/moviesaop/Hello.java
@@ -0,0 +1,15 @@
+package pl.inome.moviesaop;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+class Hello {
+
+    @EventListener(ApplicationReadyEvent.class)
+    public String sayHello() {
+        System.out.println("Hello");
+        return "Hello";
+    }
+}
