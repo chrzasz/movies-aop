e263f26c16f79b5fbc607b0d461a92a8dd74a1b3
Greg <chrzaszczyk@gmail.com>
2019-11-18 13:27:23 +0100

    simple Aspect After

--- src/main/java/pl/inome/moviesaop/HelloAspect.java
+++ src/main/java/pl/inome/moviesaop/HelloAspect.java
@@ -1,8 +1,10 @@
 package pl.inome.moviesaop;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -15,8 +17,13 @@ class HelloAspect {
         joinPoint.proceed();
     }
 
-//    @Before("execution(String pl.inome.moviesaop.Hello.sayHello())")
-//    private void beforeHello() {
-//        System.out.println("Before Hello");
-//    }
+    @Before("execution(String pl.inome.moviesaop.Hello.sayHello())")
+    private void beforeHello() {
+        System.out.println("Before Hello");
+    }
+
+    @After("execution(String pl.inome.moviesaop.Hello.sayHello())")
+    private void afterHello() {
+        System.out.println("After Hello");
+    }
 }
