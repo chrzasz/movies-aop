e9f5e8d5ded992edc84bffed45170538a22b196a
Greg <chrzaszczyk@gmail.com>
2019-11-18 13:12:19 +0100

    Initial commit

---
+++ src/main/java/pl/inome/moviesaop/MoviesAopApplication.java
@@ -0,0 +1,13 @@
+package pl.inome.moviesaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesAopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviesAopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/inome/moviesaop/MoviesAopApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.inome.moviesaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesAopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
