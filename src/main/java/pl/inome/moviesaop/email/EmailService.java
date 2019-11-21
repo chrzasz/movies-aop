15a0281ef1f6d485c2ac88449d3b115bcc11f53a
Greg <chrzaszczyk@gmail.com>
2019-11-21 14:34:32 +0100

    Email refactor

---
+++ src/main/java/pl/inome/moviesaop/email/EmailService.java
@@ -0,0 +1,6 @@
+package pl.inome.moviesaop.email;
+
+public interface EmailService {
+
+    void sendMessage(String to);
+}
---
+++ src/main/java/pl/inome/moviesaop/email/EmailServiceImp.java
@@ -0,0 +1,38 @@
+package pl.inome.moviesaop.email;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import pl.inome.moviesaop.service.MovieService;
+
+@Service
+class EmailServiceImp implements EmailService {
+
+    private JavaMailSender emailSender;
+    private MovieService movieService;
+
+    @Autowired
+    public EmailServiceImp(JavaMailSender emailSender, MovieService movieService) {
+        this.emailSender = emailSender;
+        this.movieService = movieService;
+    }
+
+    @Override
+    public void sendMessage(String to) {
+        Thread thread = new Thread(() -> {
+            SimpleMailMessage msg = new SimpleMailMessage();
+            msg.setTo(to);
+            msg.setSubject("New movie added");
+            int idx = movieService.getMovies().size();
+            msg.setText(movieService.getMovies().get(idx - 1).toString());
+            try {
+                emailSender.send(msg);
+                System.out.println("email send");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        thread.start();
+    }
+}
